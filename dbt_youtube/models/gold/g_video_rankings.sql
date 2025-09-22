9ab2ccfdc928dee54e19a508e641e7f221198b7f
tunguyenn99 <tunguyen150599@gmail.com>
2025-09-23 00:46:58 +0700

    commit

--- dbt_youtube/models/gold/g_video_rankings.sql
+++ dbt_youtube/models/gold/g_video_rankings.sql
@@ -20,6 +20,9 @@ select
   , v."VIDEO_DESC"
   , v."VIDEO_PUBLISHED_AT"::date as published_date
   , v."DURATION_SECONDS"
+  , v."VIEW_COUNT"
+  , v."LIKE_COUNT"
+  , v."COMMENT_COUNT"
   , v."LIKE_COUNT"   / nullif(v."VIEW_COUNT", 0) as like_per_view
   , v."COMMENT_COUNT"/ nullif(v."VIEW_COUNT", 0) as comment_per_view
   , row_number() over (order by v."VIEW_COUNT" desc)         as rn_by_views
